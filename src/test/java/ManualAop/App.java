3382d70f3aa585046b8579814b770a2ebd97aa6d
xuranyang <xu.ranyang@yalla.live>
2024-05-30 18:28:52 +0800

    AOP

--- src/test/java/ManualAop/App.java
+++ src/test/java/ManualAop/App.java
@@ -3,24 +3,24 @@ package ManualAop;
 public class App {
     public static void main(String[] args) {
         Hello hello = Aop.get(Hello.class);
-//        new HelloInteceptor().invoke(new Invoke() {
+//        new HelloInterceptor().invoke(new Invoke() {
 //            @Override
 //            public void invoke() {
 //                hello.test();
 //            }
 //        });
 
-//        new HelloInteceptor().invoke(() -> hello.test());
-//        new HelloInteceptor().invoke(hello::test);
+//        new HelloInterceptor().invoke(() -> hello.test());
+//        new HelloInterceptor().invoke(hello::test);
 
-//        Aop.invoke(new HelloInteceptor(), new Invoke() {
+//        Aop.invoke(new HelloInterceptor(), new Invoke() {
 //            @Override
 //            public void invoke() {
 //                hello.test();
 //            }
 //        });
 
-//        Aop.invoke(new HelloInteceptor(), () -> {
+//        Aop.invoke(new HelloInterceptor(), () -> {
 //            hello.test();
 //        });
 
