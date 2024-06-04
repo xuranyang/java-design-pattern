d292585de60cc3baacae6bf7ef2081a4c1fcfc7c
xuranyang <xu.ranyang@yalla.live>
2024-06-04 18:05:57 +0800

    Synchronized Demo

--- src/test/java/juc/SynchronizedTest1.java
+++ src/test/java/juc/SynchronizedTest1.java
@@ -3,7 +3,6 @@ package juc;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
@@ -43,10 +42,13 @@ class SafeChange implements Callable<String> {
         this.costTime = costTime;
     }
 
+    // synchronized String call() 或者 synchronized (this) 都是不行的
+    // 类锁的锁对象实际上为该类的 class对象; 而对象锁的锁对象为调用该同步方法的对应实例对象，两个对象的内存地址明显不同，所以其占有的锁资源不同
     @Override
     public String call() throws Exception {
-        // synchronized (userMap) 保证同时只能有一个线程操作 userMap对象
-        // synchronized (SafeChange.class) 或者同时只能有一个线程操作 SafeChange对象 也可以
+        // 使用 synchronized (userMap) 保证同时只能有一个线程操作 userMap对象
+        // 使用 synchronized (SafeChange.class) 也可以，同时只能有一个线程操作 SafeChange对象
+        // 但是 synchronized (this) 是不行的,等价于 synchronized String call()
         synchronized (userMap) {
             Integer balance = userMap.get(user);
             System.out.println(Thread.currentThread().getName() + "->修改前:" + balance);
